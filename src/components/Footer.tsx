813e73040ca17fa77e2f0e3f1a8af11ccd508aac
marquet <marquet@element84.com>
2021-09-28 21:32:09 -0400

    Added QuetCodesFire to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import styles from '../assets/styles/Footer.module.css'
 export default function Footer() {
   return(
     <footer className={styles.footer}>
-      &copy;2021 Marquet Reid 🔥  | All Right Reserved
+      &copy;2021 Marquet Reid | QuetCodesFire🔥  | All Right Reserved
     </footer>
   )
 }
